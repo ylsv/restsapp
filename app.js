eeba34b1103f71661ccd51df995517a5f6b97e55
ylsv <lisov1988@gmail.com>
2019-08-30 14:42:35 +0300

    add mongoose and connect it to DB, change routes accordingly

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const   express    = require('express'),
+        app        = express(),
+        bodyParser = require('body-parser'), // to be able to get input from the body of the request (req.body)
+        mongoose   = require('mongoose'); // to be able to add data to MongoDB database from our JS file
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost:27017/rests_app', {useNewUrlParser: true}); // connects mongoose to MongoDB
+app.use(bodyParser.urlencoded({extended: true})); // command to use body-parser
+app.set('view engine', 'ejs'); // lets us avoid .ejs endings in files
 
-const restaurants = [
-    {name: 'Ruski', image: 'https://source.unsplash.com/S9yn7XYqxoU/800x600'},
-    {name: 'I like wine', image: 'https://source.unsplash.com/lZrLPITHbxU/800x600'},
-    {name: 'Erwin. РекаМореОкеан', image: 'https://source.unsplash.com/_P8hIt5lTUA/800x600'},
-    {name: 'Ruski', image: 'https://source.unsplash.com/S9yn7XYqxoU/800x600'},
-    {name: 'I like wine', image: 'https://source.unsplash.com/lZrLPITHbxU/800x600'},
-    {name: 'Erwin. РекаМореОкеан', image: 'https://source.unsplash.com/_P8hIt5lTUA/800x600'},
-    {name: 'Ruski', image: 'https://source.unsplash.com/S9yn7XYqxoU/800x600'},
-    {name: 'I like wine', image: 'https://source.unsplash.com/lZrLPITHbxU/800x600'},
-    {name: 'Erwin. РекаМореОкеан', image: 'https://source.unsplash.com/_P8hIt5lTUA/800x600'},
-    {name: 'Ruski', image: 'https://source.unsplash.com/S9yn7XYqxoU/800x600'},
-    {name: 'I like wine', image: 'https://source.unsplash.com/lZrLPITHbxU/800x600'},
-    {name: 'Erwin. РекаМореОкеан', image: 'https://source.unsplash.com/_P8hIt5lTUA/800x600'},
-];
+// MONGOOSE SCHEMA SETUP
+// creating schema:
+const restSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+// creating restaurant model based on our schema allowing us to use methods:
+const Restaurant = mongoose.model('Restaurant', restSchema);
+
+/*Restaurant.create(
+    {
+        name: 'I like wine',
+        image: 'https://source.unsplash.com/lZrLPITHbxU/800x600'
+    }, function(err, rest){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('NEWLY CREATED RESTAURANT:');
+            console.log(rest);
+        }
+    });*/
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
 app.get('/restaurants', function(req, res){
-    res.render('restaurants', {restaurants: restaurants});
+    // Get all restaurants from DB and render them
+    Restaurant.find({}, function(err, allRests){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('restaurants', {restaurants: allRests});
+        }
+    });
 });
 
 app.get('/restaurants/new', function(req, res){
@@ -37,9 +53,15 @@ app.post('/restaurants', function(req, res){
     const name = req.body.name;
     const image = req.body.image;
     const newRestaurant = {name: name, image: image};
-    restaurants.push(newRestaurant);
-    //redirect back to rests page
-    res.redirect('/restaurants');
+    // Create a new rest and save it to DB
+    Restaurant.create(newRestaurant, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to rests page
+            res.redirect('/restaurants');
+        }
+    });
 });
 
 
